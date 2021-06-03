6f811a34727cc3b4af20efdb5407163a03d8103f
Isaac Cavalcante <isaac.cavalcante@monitoratec.com.br>
2021-06-03 11:57:19 -0300

    improve test name and test error message

--- DataTests/RemoteAddAccountTests.swift
+++ DataTests/RemoteAddAccountTests.swift
@@ -21,12 +21,12 @@ protocol HttpPostClient {
 
 class DataTests: XCTestCase {
 
-    func test_() throws {
+    func test_add_should_call_httpClient_with_correct_url() throws {
         let url = URL(string: "https://any-url.com")!
         let httpClientSpy = HttpClientSpy()
         let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
         sut.add()
-        XCTAssertEqual(httpClientSpy.url, url)
+        XCTAssertEqual(httpClientSpy.url, url, "Url send to httpClient is wrong")
     }
     
     class HttpClientSpy: HttpPostClient {
