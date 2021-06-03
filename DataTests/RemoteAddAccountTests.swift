9b52af9154e4a38e7fcb36e1cd4e902f6f92218b
Isaac Cavalcante <isaac.cavalcante@monitoratec.com.br>
2021-06-03 14:13:04 -0300

    AddAccountModel creation, RemotAddAccount, URL and HttpPostClient moved to factory methods

--- DataTests/RemoteAddAccountTests.swift
+++ DataTests/RemoteAddAccountTests.swift
@@ -24,27 +24,36 @@ protocol HttpPostClient {
 class DataTests: XCTestCase {
 
     func test_add_should_call_httpClient_with_correct_url() throws {
-        let url = URL(string: "https://any-url.com")!
-        let httpClientSpy = HttpClientSpy()
-        let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
-        let addAccountModel = AddAccountModel(name: "anyName", email: "anyEmail", password: "anyPassword", passwordConfirmation: "anyPassword")
+        let (sut, url, httpClientSpy) = makeSut()
+        let addAccountModel = makeAddAccountModel()
         sut.add(addAccountModel: addAccountModel)
         XCTAssertEqual(httpClientSpy.url, url, "Url send to httpClient is wrong")
     }
     
     func test_add_should_call_httpClient_with_correct_data() throws {
-        let httpClientSpy = HttpClientSpy()
-        let sut = RemoteAddAccount(url: URL(string: "https://any-url.com")!, httpClient: httpClientSpy)
-        let addAccountModel = AddAccountModel(name: "anyName", email: "anyEmail", password: "anyPassword", passwordConfirmation: "anyPassword")
-        sut.add(addAccountModel: addAccountModel)
+        let sut = makeSut()
+        let addAccountModel = makeAddAccountModel()
+        sut.sut.add(addAccountModel: addAccountModel)
         
         let data = try? JSONEncoder().encode(addAccountModel)
-        XCTAssertEqual(httpClientSpy.data, data, "Data from to httpClient is wrong")
+        XCTAssertEqual(sut.httpClientSpy.data, data, "Data from to httpClient is wrong")
     }
     
 }
 
 extension DataTests {
+    func makeSut() -> (sut: RemoteAddAccount, url: URL, httpClientSpy: HttpClientSpy){
+        let url = URL(string: "https://any-url.com")!
+        let httpClientSpy = HttpClientSpy()
+        let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
+        
+        return (sut: sut, url: url, httpClientSpy: httpClientSpy)
+    }
+    
+    func makeAddAccountModel () -> AddAccountModel {
+        return AddAccountModel(name: "anyName", email: "anyEmail", password: "anyPassword", passwordConfirmation: "anyPassword")
+    }
+    
     class HttpClientSpy: HttpPostClient {
         var url: URL?
         var data: Data?
