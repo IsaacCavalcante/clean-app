49d00cea11e2aa552b1c2999852905c695ba7029
Isaac Cavalcante <isaac.cavalcante@monitoratec.com.br>
2021-06-08 11:52:03 -0300

    move duplicated code to a helper method to improve readability

--- InfraTests/AlamofireAdapterTest.swift
+++ InfraTests/AlamofireAdapterTest.swift
@@ -16,64 +16,22 @@ class AlamofireAdapterTest: XCTestCase {
         }
     }
 
-
     func test_post_should_make_request_with_valid_url_and_method() {
         let url = makeUrl()
-        let sut = makeSut().sut
-        
         let data = makeValidData()
-        sut.post(to: url, with: data){_  in }
         
-        let exp = expectation(description: "completion to add remote account should response until 1 second")
-        URLProtocolStub.observeRequest { request in
+        testRequestFor(data: data) { request in
             XCTAssertEqual(url, request.url, "Url send to AlamofireAdapter is wrong")
             XCTAssertEqual("POST", request.httpMethod, "Http method calls by AlamofireAdapter is wrong")
             XCTAssertNotNil(request.httpBodyStream)
-            exp.fulfill()
         }
-        wait(for: [exp], timeout: 1)
     }
     
     func test_post_should_make_request_with_no_data() {
-        let sut = makeSut().sut
-        
-        sut.post(to: makeUrl(), with: nil){_  in }
-        
-        let exp = expectation(description: "completion to add remote account should response until 1 second")
-        URLProtocolStub.observeRequest { request in
+        testRequestFor(data: nil) { request in
             XCTAssertNil(request.httpBodyStream)
-            exp.fulfill()
         }
-        wait(for: [exp], timeout: 1)
     }
-    
-    //Essa classe URLProtocol intercepta as requests que são feitas. Aqui usamos para testar as nossas requests
-    class URLProtocolStub: URLProtocol {
-        static var emit: ((URLRequest) -> Void)?
-        
-        static func observeRequest(completion: @escaping (URLRequest) -> Void) {
-            URLProtocolStub.emit = completion
-        }
-        
-        open override class func canInit(with request: URLRequest) -> Bool {
-            //return true define que todas as urls requisitadas serão interceptadas
-            return true
-        }
-        
-        open override class func canonicalRequest(for request: URLRequest) -> URLRequest {
-            return request
-        }
-        
-        open override func startLoading() {
-            URLProtocolStub.emit?(request)
-        }
-        
-        open override func stopLoading() {
-            
-        }
-
-    }
-
 }
 
 extension AlamofireAdapterTest {
@@ -85,5 +43,43 @@ extension AlamofireAdapterTest {
         let sut = AlamofireAdapter(session: session)
         
         return (sut: sut, session: session)
+    }
+    
+    func testRequestFor(url: URL = makeUrl(), data: Data?, action: @escaping (URLRequest) -> Void) {
+        let sut = makeSut().sut
+        sut.post(to: url, with: data){_  in }
+        
+        let exp = expectation(description: "completion to add remote account should response until 1 second")
+        URLProtocolStub.observeRequest { request in
+            action(request)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
+    }
+}
+
+//Essa classe URLProtocol intercepta as requests que são feitas. Aqui usamos para testar as nossas requests
+class URLProtocolStub: URLProtocol {
+    static var emit: ((URLRequest) -> Void)?
+    
+    static func observeRequest(completion: @escaping (URLRequest) -> Void) {
+        URLProtocolStub.emit = completion
+    }
+    
+    open override class func canInit(with request: URLRequest) -> Bool {
+        //return true define que todas as urls requisitadas serão interceptadas
+        return true
+    }
+    
+    open override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    open override func startLoading() {
+        URLProtocolStub.emit?(request)
+    }
+    
+    open override func stopLoading() {
+        
     }
 }
