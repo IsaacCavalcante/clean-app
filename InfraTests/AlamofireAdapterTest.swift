126328295e3a908af004df8ed8e30323fb5bea83
Isaac Cavalcante <isaac.cavalcante@monitoratec.com.br>
2021-06-08 22:48:34 -0300

    create helper to improve code readability

--- InfraTests/AlamofireAdapterTest.swift
+++ InfraTests/AlamofireAdapterTest.swift
@@ -11,11 +11,11 @@ class AlamofireAdapterTest: XCTestCase {
             self.session = session
         }
         
-        func post(to url: URL, with data: Data? ,completion: @escaping (Result<AnyObject,HttpError>) -> Void) {
+        func post(to url: URL, with data: Data? ,completion: @escaping (Result<Data,HttpError>) -> Void) {
             session.request(url, method: .post, parameters: data?.toJson(), encoding: JSONEncoding.default).responseData { dataResponse in
                 switch dataResponse.result {
                 case .failure: completion(.failure(.noConnectivity))
-                case .success(_): break
+                case .success: break
                 }
             }
         }
@@ -39,17 +39,7 @@ class AlamofireAdapterTest: XCTestCase {
     }
     
     func test_post_should_complete_with_error_when_request_complete_with_error() {
-        let sut = makeSut().sut
-        URLProtocolStub.simulate(data: nil, response: nil, error: makeError())
-        let exp = expectation(description: "completion to add remote account should response until 1 second")
-        sut.post(to: makeUrl(), with: makeValidData()) { result in
-            switch result{
-            case .failure(let error): XCTAssertEqual(error, .noConnectivity)
-            case .success: XCTFail("Expected error \(result) insted")
-            }
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1)
+        expectResult(.failure(.noConnectivity), when: (data: nil, response: nil, error: makeError()))
     }
 }
 
@@ -74,6 +64,23 @@ extension AlamofireAdapterTest {
         URLProtocolStub.observeRequest { request = $0 }
         wait(for: [exp], timeout: 1)
         action(request!)
+    }
+    
+    func expectResult(_ expectedResult: Result<Data, HttpError>, when stub: (data: Data?, response: HTTPURLResponse?, error: Error?), file: StaticString = #filePath, line: UInt = #line) {
+        let sut = makeSut().sut
+        URLProtocolStub.simulate(data: stub.data, response: stub.response, error: stub.error)
+        let exp = expectation(description: "completion to add remote account should response until 1 second")
+        
+        sut.post(to: makeUrl(), with: makeValidData()){ receivedResult in
+            switch (expectedResult, receivedResult) {
+            case (.failure(let expectedError), .failure(let receivedError)): XCTAssertEqual(expectedError, receivedError, file: file, line: line)
+            case (.success(let expectedData), .success(let receivedData)): XCTAssertEqual(expectedData, receivedData, file: file, line: line)
+            default: XCTFail("Expected \(expectedResult), but received \(receivedResult) insted")
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1)
     }
 }
 
