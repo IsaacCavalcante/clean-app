1cdfa69ea8d04f18fd32c47e0ab3f7766eb5126e
Isaac Cavalcante <isaac.cavalcante@monitoratec.com.br>
2021-06-10 00:09:25 -0300

    ensure SignupPresenter show erro if no name is provided

--- PresentationTests/SignupPresenterTests.swift
+++ PresentationTests/SignupPresenterTests.swift
@@ -1,5 +1,53 @@
 import XCTest
+
+class SignupPresenter {
+    private let alertView: AlertView?
+    
+    init(alertView: AlertView) {
+        self.alertView = alertView
+    }
+    
+    func signUp(viewModel: SignupViewModel) {
+        if (viewModel.name == nil || viewModel.name!.isEmpty) {
+            alertView?.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: "Nome é obrigatório"))
+        }
+    }
+}
+
+protocol AlertView {
+    func showMessage(viewModel: AlertViewModel)
+}
+
+struct AlertViewModel: Equatable {
+    var title: String
+    var message: String
+}
+
+struct SignupViewModel {
+    var name: String?
+    var email: String?
+    var password: String?
+    var passwordConfirmation: String?
+}
+
+
 class SignupPresenterTests: XCTestCase {
-    func test_() {
+    func test_signup_should_show_error_message_if_name_is_not_provider() {
+        let alertViewSpy = AlertViewSpy()
+        let sut = SignupPresenter(alertView: alertViewSpy)
+        let signupViewModel = SignupViewModel(email: "any@mail.com", password: "password", passwordConfirmation: "password")
+        sut.signUp(viewModel: signupViewModel)
+        
+        XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Falha na validação", message: "Nome é obrigatório"))
+    }
+}
+
+extension SignupPresenterTests {
+    class AlertViewSpy: AlertView {
+        var viewModel: AlertViewModel?
+        
+        func showMessage(viewModel: AlertViewModel) {
+            self.viewModel = viewModel
+        }
     }
 }
