9e1fcd82a26b3df70ae4ddb9bc54e64452608ce2
Isaac Cavalcante <isaac.cavalcante@monitoratec.com.br>
2021-06-10 11:59:08 -0300

    move validation to a method

--- PresentationTests/SignupPresenterTests.swift
+++ PresentationTests/SignupPresenterTests.swift
@@ -8,15 +8,22 @@ class SignupPresenter {
     }
     
     func signUp(viewModel: SignupViewModel) {
-        if (viewModel.name == nil || viewModel.name!.isEmpty) {
-            alertView?.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: "Nome é obrigatório"))
-        } else if (viewModel.email == nil || viewModel.email!.isEmpty) {
-            alertView?.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: "Email é obrigatório"))
-        } else if (viewModel.password == nil || viewModel.password!.isEmpty) {
-            alertView?.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: "Senha é obrigatória"))
-        } else if (viewModel.passwordConfirmation == nil || viewModel.passwordConfirmation!.isEmpty) {
-            alertView?.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: "Confirmação de senha é obrigatória"))
+        if let message = validate(viewModel: viewModel) {
+            alertView?.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: message))
         }
+    }
+    
+    private func validate(viewModel: SignupViewModel) -> String? {
+        if (viewModel.name == nil || viewModel.name!.isEmpty) {
+            return "Nome é obrigatório"
+        } else if (viewModel.email == nil || viewModel.email!.isEmpty) {
+            return "Email é obrigatório"
+        } else if (viewModel.password == nil || viewModel.password!.isEmpty) {
+            return "Senha é obrigatória"
+        } else if (viewModel.passwordConfirmation == nil || viewModel.passwordConfirmation!.isEmpty) {
+            return "Confirmação de senha é obrigatória"
+        }
+        return nil
     }
 }
 
