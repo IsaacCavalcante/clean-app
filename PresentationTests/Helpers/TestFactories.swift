75c00918972e06d787390f3d32c86680f203c788
Isaac Cavalcante <isaac.cavalcante@monitoratec.com.br>
2021-06-11 15:49:05 -0300

    factories methods move to its own file

---
+++ PresentationTests/Helpers/TestFactories.swift
@@ -0,0 +1,17 @@
+//
+//  TestFactories.swift
+//  PresentationTests
+//
+//  Created by monitora on 11/06/21.
+//
+
+import Foundation
+import Presentation
+
+func makeSignupViewModel(name: String? = "Isaac Cavalcante", email: String? = "any@mail.com", password: String? = "password", passwordConfirmation: String? = "password") -> SignupViewModel {
+    return SignupViewModel(name: name, email: email, password: password, passwordConfirmation: passwordConfirmation)
+}
+
+func makeAlertViewModel(title: String? = "Falha na validação", message: String? = "Falha na validação") -> AlertViewModel {
+    return AlertViewModel(title: title!, message: message!)
+}
--- PresentationTests/Signup/SignupPresenterTests.swift
+++ PresentationTests/Signup/SignupPresenterTests.swift
@@ -185,12 +185,4 @@ extension SignupPresenterTests {
         
         return (sut, alertViewSpy, loadingViewSpy, emailValidatorSpy, addAccountSpy)
     }
-    
-    func makeSignupViewModel(name: String? = "Isaac Cavalcante", email: String? = "any@mail.com", password: String? = "password", passwordConfirmation: String? = "password") -> SignupViewModel {
-        return SignupViewModel(name: name, email: email, password: password, passwordConfirmation: passwordConfirmation)
-    }
-
-    func makeAlertViewModel(title: String? = "Falha na validação", message: String? = "Falha na validação") -> AlertViewModel {
-        return AlertViewModel(title: title!, message: message!)
-    }
 }
