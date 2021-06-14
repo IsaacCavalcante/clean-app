785fa638f9987b95a502cffe6135fcd893b69c71
Isaac Cavalcante <isaac.cavalcante@monitoratec.com.br>
2021-06-14 18:02:33 -0300

    move WeakVarProxy to its own file

--- Main/Factories/SignupFactory.swift
+++ Main/Factories/SignupFactory.swift
@@ -8,7 +8,7 @@ class ControllerFactory {
     static func makeSignup(addAccount: AddAccount) -> SignupViewController {
         let controller = SignupViewController.instantiate()
         let emailValidatorAdapter = EmailValidatorAdapter()
-        let presenter = SignupPresenter(alertView: controller, emailValidator: emailValidatorAdapter, addAccount: addAccount, loadingView: controller)
+        let presenter = SignupPresenter(alertView: WeakVarProxy(controller), emailValidator: emailValidatorAdapter, addAccount: addAccount, loadingView: WeakVarProxy(controller))
         controller.signUp = presenter.signUp
         
         return controller
