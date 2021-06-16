848679cb41d1d68b22e15b93c67df0bdad4b6cfb
Isaac Cavalcante <isaac.cavalcante@monitoratec.com.br>
2021-06-16 16:03:35 -0300

    change test file name

--- MainTests/Composers/SignupComposerTests.swift
+++ MainTests/Composers/SignupComposerTests.swift
@@ -2,11 +2,11 @@ import XCTest
 import Main
 import Data
 
-class SignupIntegrationTests: XCTestCase {
+class SignupComposerTests: XCTestCase {
     func test_ui_presentantion_integration() {
-        debugPrint("===============================")
-        debugPrint(Environment.variable(.apiBaseUrl))
-        debugPrint("===============================")
+//        debugPrint("===============================")
+//        debugPrint(Environment.variable(.apiBaseUrl))
+//        debugPrint("===============================")
         let sut = SignupComposer.composeControllerWith(addAccount: AddAccountSpy())
         checkMemoryLeak(for: sut)
     }
