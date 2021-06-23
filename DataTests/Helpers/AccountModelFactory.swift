cd074ef9e42dd86d4acdf9b428444a70171d36ba
Isaac Cavalcante <isaac.cavalcante@monitoratec.com.br>
2021-06-23 09:24:42 -0300

    change AddAccount to have only an accessToken

--- DataTests/Helpers/AccountModelFactory.swift
+++ DataTests/Helpers/AccountModelFactory.swift
@@ -2,5 +2,5 @@ import Foundation
 import Domain
 
 func makeAccountModel() -> AccountModel {
-    return AccountModel(id: "someId", name: "anyName", email: "anyEmail", password: "anyPassword")
+    return AccountModel(accessToken: "any_token")
 }
