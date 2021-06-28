dc5c6f90e952dd793edf697e202bfe20634f3e1f
Isaac Cavalcante <isaac.cavalcante@monitoratec.com.br>
2021-06-28 13:16:34 -0300

    ensure SigninPresenter shows error message if validation fails

--- Presentation/Presenters/Signin/SigninPresenter.swift
+++ Presentation/Presenters/Signin/SigninPresenter.swift
@@ -3,12 +3,16 @@ import Domain
 
 public final class SigninPresenter {
     private let validation: Validation
+    private let alertView: AlertView
     
-    public init(validation: Validation) {
+    public init(validation: Validation, alertView: AlertView) {
         self.validation = validation
+        self.alertView = alertView
     }
     
     public func signIn(viewModel: SigninViewModel) {
-        _ = validation.validate(data: viewModel.toJson())
+        if let message = validation.validate(data: viewModel.toJson()) {
+            alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: message))
+        }
     }
 }
--- PresentationTests/Presenters/Signin/SigninPresenterTests.swift
+++ PresentationTests/Presenters/Signin/SigninPresenterTests.swift
@@ -11,15 +11,32 @@ class SigninPresenterTests: XCTestCase {
         sut.signIn(viewModel: viewModel)
         XCTAssertTrue(NSDictionary(dictionary: validation.data!).isEqual(to: viewModel.toJson()!))
     }
+    
+    func test_signin_should_show_error_message_if_validation_fails() {
+        let test = makeSut()
+        let sut = test.sut
+        let alertViewSpy = test.alertViewSpy
+        let validationSpy = test.validationSpy
+        
+        let exp = expectation(description: "completion to add remote account should response until 1 second")
+        alertViewSpy.observer { viewModel in
+            XCTAssertEqual(viewModel, makeAlertViewModel(message: "Erro"))
+            exp.fulfill()
+        }
+        validationSpy.simulateError()
+        sut.signIn(viewModel: makeSigninViewModel())
+        wait(for: [exp], timeout: 1)
+    }
 }
 
 
 extension SigninPresenterTests {
-    func makeSut(file: StaticString = #filePath, line: UInt = #line) -> (sut: SigninPresenter, validationSpy: ValidationSpy) {
+    func makeSut(file: StaticString = #filePath, line: UInt = #line) -> (sut: SigninPresenter, alertViewSpy: AlertViewSpy, validationSpy: ValidationSpy) {
         let validationSpy = ValidationSpy()
-        let sut = SigninPresenter(validation: validationSpy)
+        let alertViewSpy = AlertViewSpy()
+        let sut = SigninPresenter(validation: validationSpy, alertView: alertViewSpy)
         checkMemoryLeak(for: sut, file: file, line: line)
         
-        return (sut, validationSpy)
+        return (sut, alertViewSpy, validationSpy)
     }
 }
