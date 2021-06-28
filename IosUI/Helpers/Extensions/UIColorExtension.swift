c1cb7a60bb7f5709d513ae76234af533a3196c35
Isaac Cavalcante <isaac.cavalcante@monitoratec.com.br>
2021-06-28 16:46:13 -0300

    ensure animate and hide display if display() and show message when showMessage() are called

--- IosUI/Helpers/Extensions/UIColorExtension.swift
+++ IosUI/Helpers/Extensions/UIColorExtension.swift
@@ -1,19 +1,24 @@
 import UIKit
 
 extension UIColor {
-   convenience init(red: Int, green: Int, blue: Int) {
-       assert(red >= 0 && red <= 255, "Invalid red component")
-       assert(green >= 0 && green <= 255, "Invalid green component")
-       assert(blue >= 0 && blue <= 255, "Invalid blue component")
-
-       self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
-   }
-
-   convenience init(rgb: Int) {
-       self.init(
-           red: (rgb >> 16) & 0xFF,
-           green: (rgb >> 8) & 0xFF,
-           blue: rgb & 0xFF
-       )
-   }
+    convenience init(red: Int, green: Int, blue: Int) {
+        var redColor = red > 255 ? 255 : red
+        redColor = red < 0 ? 0 : red
+        
+        var greenColor = green > 255 ? 255 : green
+        greenColor = green < 0 ? 0 : green
+        
+        var blueColor = blue > 255 ? 255 : blue
+        blueColor = blue < 0 ? 0 : blue
+        
+        self.init(red: CGFloat(redColor) / 255.0, green: CGFloat(greenColor) / 255.0, blue: CGFloat(blueColor) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
 }
